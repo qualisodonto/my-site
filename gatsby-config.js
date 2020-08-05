1b5b11c91c90afccc8ff62a4458dbade2da8fbdb
Pedro Teixeira <pedrao@gmail.com>
2020-08-05 11:46:54 -0300

    Clean up starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Qualis Odontologia Integrada`,
+    description: `Website da clínica Qualis Odontologia Integrada`,
+    author: `@lambdapetrus`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
